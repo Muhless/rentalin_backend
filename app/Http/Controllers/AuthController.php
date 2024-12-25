dcae70b81f794e1559a4f053fcde9dadf66c7c3a
Muhless <nuryadi.muhta@gmail.com>
2024-12-25 08:37:28 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,39 +12,45 @@ use Laravel\Sanctum\HasApiTokens;
 
 class AuthController extends Controller
 {
-    public function showRegistrationForm()
-    {
-        return view('auth.register');
-    }
 
-    function register(Request $request)
+    public function register(Request $request)
     {
-        try {
-            $cred = new User();
-            $cred->name = $request->name;
-            $cred->email = $request->email;
-            $cred->password = Hash::make($request->password);
-            $cred->save();
-            $response = ['status' => 200, 'message' => 'User created successfully'];
-            return response()->json($response);
-        } catch (Exception $e) {
-            $response = ['status ' => 500, 'message' => 'User creation failed'];
+        // validasi
+        $rules = [
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users',
+            'password' => 'required|string|min:6'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
         }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $token = $user->createToken('personal access token')->plainTextToken;
+        $response = ['user' => $user, 'token' => $token];
+        return response()->json($response, 201);
     }
 
-    function Login(Request $request){
+    function Login(Request $request)
+    {
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ];
+        $request->validate($rules);
         $user = User::where('email', $request->email)->first();
-
-        if ($user!= '[]' && Hash::check($request->password, $user->password)) {
-            $token = $user->createToken('token')->plainTextToken;
-            $response = ['status' => 200, 'token' => $token, 'user' => $user, 'message' => 'User logged in successfully'];
-            return response()->json($response);
-        } else if ($user == '[]') {
-            $response = ['status' => 500, 'message' => 'User not found'];
-            return response()->json($response);
-        } else {
-            $response = ['status' => 500, 'message' => 'wrong email or password'];
-            return response()->json($response);
+        if ($user && hash::check($request->password, $user->password)) {
+            $token = $user->createToken('personal access token')->plainTextToken;
+            $response = ['user' => $user, 'token' => $token];
+            return response()->json($response, 200);
         }
+        $response = ['message' => 'Invalid email or password'];
+        return response()->json($response, 400);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/users', [DataController::class, 'index']);
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
